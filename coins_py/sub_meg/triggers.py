c222a9277897cddef8a68722b944c7631d4d27df
Yulia <49763467+juli-ter@users.noreply.github.com>
2026-04-20 15:21:58 +0200

    Create triggers.py

---
+++ coins_py/sub_meg/triggers.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+def coins_meg_trigger_list() -> dict[str, int]:
+    return {
+        'expStart': 100,
+        'practiceMove': 101,
+        'practiceSize': 102,
+        'expEnd': 105,
+        'lastFrame': 99,
+        'blockStart': 10,
+        'blockEnd': 20,
+        'laserHit': 1,
+        'laserMiss': 2,
+        'keyRight': 3,
+        'keyLeft': 4,
+        'keyUp': 5,
+        'keyDown': 6,
+        'keyRelease': 7,
+    }
